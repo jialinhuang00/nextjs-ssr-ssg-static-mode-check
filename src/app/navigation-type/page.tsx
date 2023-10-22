a79d714b694e24eba2465b723fbb4d7d0121ba40
jialin <jialin@confetti.studio>
2023-10-22 21:13:11 +0800

    chore: refactor term

--- src/app/navigation-type/page.tsx
+++ src/app/navigation-type/page.tsx
@@ -6,12 +6,16 @@ export default function About() {
       <Link href="/navigation-type/client-side-about">
         Client-Side Navigation Page by `next/link`
       </Link>
-      <a href="/navigation-type/ssr-about">
-        Go to SSR about Page by `html anchor tag`
+      <a href="/navigation-type/traditional-navigation-about">
+        Go to Traditional Navigation about Page by `html anchor tag`
       </a>
 
       <h1 className="text-3xl">How?</h1>
       <p>Open Console, let `html` be red background, then click these two</p>
+      <p>
+        If Javascript disabled, `Client-Side Navigation` will fallback to
+        `Traditional Navigation`
+      </p>
       <p>Inspiration here:</p>
       <a href="https://stackoverflow.com/questions/64644248/next-js-client-side-navigation-vs-changes-in-html">
         Next.js - Client Side Navigation vs. changes in html
--- src/app/navigation-type/traditional-navigation-about/page.tsx
+++ src/app/navigation-type/traditional-navigation-about/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-
-export default async function ClientSideNavigationAboutPage() {
+export default async function TraditionalAboutPage() {
   const serverData = await getthejsontestdata();
   return (
     <div className="flex flex-col gap-4 p-8">
-      <p>This is `Client Side Navigation` About Page</p>
-      <Link href="/navigation-type">Back to /navigation-type</Link>
+      <p>This is `Traditional Navigaiton` About page</p>
+      <a href="/navigation-type">Back to /navigation-type also by ssr</a>
       <pre className="max-w-[480px] whitespace-pre-wrap bg-[#fafafa]">
         {JSON.stringify(serverData)}
       </pre>
@@ -17,5 +15,6 @@ const getthejsontestdata = async () => {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/posts/1/comments"
   );
+
   return res.json();
 };
