34f161d5e1e345b1f3db8772578e478c7610d617
jialin <jialin@confetti.studio>
2023-10-22 21:25:54 +0800

    refactor: compare with prefetch

---
+++ src/app/navigation-type/client-side-no-prefetch-about/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default async function ClientSideNavigationNoPrefetchAboutPage() {
+  const serverData = await getthejsontestdata();
+  return (
+    <div className="flex flex-col gap-4 p-8">
+      <p>This is `Client Side With No prefetch Navigation` About Page</p>
+      <Link href="/navigation-type">Back to /navigation-type</Link>
+      <pre className="max-w-[480px] whitespace-pre-wrap bg-[#fafafa]">
+        {JSON.stringify(serverData)}
+      </pre>
+    </div>
+  );
+}
+
+const getthejsontestdata = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments"
+  );
+  return res.json();
+};
--- src/app/navigation-type/page.tsx
+++ src/app/navigation-type/page.tsx
@@ -6,8 +6,22 @@ export default function About() {
       <Link href="/navigation-type/client-side-about">
         Client-Side Navigation Page by `next/link`
       </Link>
+      <p className="text-xs mb-4">
+        because default prefetch in next/link is true, it will also prefetch
+        /navigation-type/client-side-about when loaded this page
+      </p>
+      <Link
+        href="/navigation-type/client-side-no-prefetch-about"
+        prefetch={false}
+      >
+        Client-Side Navigation Page by `next/link prefetch=false`
+      </Link>
+      <p className="text-xs mb-4">
+        prefetch is false, it WONT prefetch when loaded this page, but still act
+        like CSN
+      </p>
       <a href="/navigation-type/traditional-navigation-about">
-        Go to Traditional Navigation about Page by `html anchor tag`
+        Go to Traditional Navigation Page by `html anchor tag`
       </a>
 
       <h1 className="text-3xl">How?</h1>
